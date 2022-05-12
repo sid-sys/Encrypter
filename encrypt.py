6539ed89086ed86be6f309a684f9f75957814345
Sidharth N Babu <sidharthbabu9@gmail.com>
2022-05-12 22:57:24 +0530

    code edited

--- encrypt.py
+++ encrypt.py
@@ -7,11 +7,12 @@ key = b'Tj1IKVVTV1hiNDdheV1ZSnd-Wjc3QzcjVVNRaFwjWkQ='
 if st.button("Encrypt"):
     encMessage = Fernet(key).encrypt(message.encode())
     encMessage = encMessage.decode("utf-8") 
-    st.write("Your encrypted message:\n ", encMessage)
+    st.write("Your Encrypted message:")
+    st.write(encMessage)
 
-st.write("\n\n")
-EncMessage =  st.text_input("Enter Your Encypted Message")
+EncMessage =  st.text_input("Enter Encypted Message")
 if st.button("Decrypt"):
     EncMessage =  EncMessage.encode("utf-8") 
     decMessage = Fernet(key).decrypt(EncMessage).decode()
-    st.write("Your Decrypted string: \n", decMessage)
+    st.write("Your Decrypted message:")
+    st.write(decMessage)
